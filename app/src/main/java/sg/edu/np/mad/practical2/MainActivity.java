4dde8f5e0de9a965062750746f7aa48fcd425bb5
Xin Yin <xinyin322@gmail.com>
2023-05-01 21:24:15 +0800

    Fix follow button, added scrollview

--- app/src/main/java/sg/edu/np/mad/practical2/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/practical2/MainActivity.java
@@ -13,6 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        User user = new User ("XinYin", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua bfhewjdqwfbehqdwnkjbfehdwkdjbcqdwnjbfhewjdqwfbehqdwnkjbfehdwkdjbcqdwnjbfhewjdqwfbehqdwnkjbfehdwkdjbcqdwnjbfhewjdqwfbehqdwnkjbfehdwkdjbcqdwnjbfhewjdqwfbehqdwnkjbfehdwkdjbcqdwnjbfhewjdqwfbehqdwnkjbfehdwkdjbcqdwnj.", 1, true);
+
+        TextView name = findViewById(R.id.textView2);
+        name.setText(user.name);
+
+        TextView desc = findViewById(R.id.textView);
+        desc.setText((user.description));
+
+        Button followBtn = findViewById(R.id.button);
+        followBtn.setOnClickListener(v -> {
+            user.followed = !user.followed;
+            followBtn.setText(user.followed ? "UNFOLLOW" : "FOLLOW");
+        });
     }
 
     /*@Override
